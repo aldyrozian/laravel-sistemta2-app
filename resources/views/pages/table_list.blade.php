16d450e185a994de7a2f96d9e2fad486e089420f
aldyrozian <aldyshimozikato@gmail.com>
2022-09-20 01:58:34 +0700

    back update

--- resources/views/pages/table_list.blade.php
+++ resources/views/pages/table_list.blade.php
@@ -5,9 +5,57 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-12">
-        {!! Form::open(['url' => 'foo/bar']) !!}
-            
-        {!! Form::close() !!}
+
+{{-- Card form pendaftaran --}}
+
+        <div class="card ">
+              <div class="card-header card-header-primary">
+                <h4 class="card-title">{{ __('Pendaftaran Tugas Akhir 2') }}</h4>
+                <p class="card-category">{{ __('masukann data data dibawah ini untuk memenuhi persyaratan tugas akhir 2') }}</p>
+              </div>
+              <div class="card-body ">
+                @if (session('status'))
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="alert alert-success">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <i class="material-icons">close</i>
+                        </button>
+                        <span>{{ session('status') }}</span>
+                      </div>
+                    </div>
+                  </div>
+                @endif
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">{{ __('Name') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                      <input class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" id="input-name" type="text" placeholder="{{ __('Name') }}" value="{{ old('name', auth()->user()->name) }}" required="true" aria-required="true"/>
+                      @if ($errors->has('name'))
+                        <span id="name-error" class="error text-danger" for="input-name">{{ $errors->first('name') }}</span>
+                      @endif
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">{{ __('Email') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                      <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" id="input-email" type="email" placeholder="{{ __('Email') }}" value="{{ old('email', auth()->user()->email) }}" required />
+                      @if ($errors->has('email'))
+                        <span id="email-error" class="error text-danger" for="input-email">{{ $errors->first('email') }}</span>
+                      @endif
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card-footer ml-auto mr-auto">
+                <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
+              </div>
+            </div>
+
+{{-- akhir form pendaftaran --}}
+
       </div>
     </div>
   </div>
