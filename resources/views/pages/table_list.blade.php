59d7d1bab0fe700c9f1a1d9b6c60c556eef7d62d
aldyrozian <aldyshimozikato@gmail.com>
2022-09-20 17:43:45 +0700

    sidebar repair and newform

--- resources/views/pages/table_list.blade.php
+++ resources/views/pages/table_list.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['activePage' => 'table', 'titlePage' => __('Table List')])
+@extends('layouts.app', ['activePage' => 'table', 'titlePage' => __('Pendaftaran Tugas Akhir 2')])
 
 @section('content')
 <div class="content">
